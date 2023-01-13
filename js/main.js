0ccd4d18485b00d471d06d89480b0b0bb173f584
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-13 16:46:09 +0200

    Trip-section-done
    Trip-section-done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-    $('.fairy-tale__slider').slick({});
+$(function () {
+    $('.fairy-tale__slider, .trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-right.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 });
